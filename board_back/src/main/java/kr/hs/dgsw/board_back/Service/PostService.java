ab99f8a826501380fbcfd3054c36c601ecd2cd4c
Ruben Hur <2001.hcy@gmail.com>
2019-06-03 15:45:51 +0900

    remove unused import

--- board_back/src/main/java/kr/hs/dgsw/board_back/Service/PostService.java
+++ board_back/src/main/java/kr/hs/dgsw/board_back/Service/PostService.java
@@ -1,9 +1,6 @@
 package kr.hs.dgsw.board_back.Service;
 
 import kr.hs.dgsw.board_back.Domain.Post;
-import kr.hs.dgsw.board_back.Domain.User;
-
-import java.util.HashMap;
 import java.util.List;
 
 public interface PostService {
